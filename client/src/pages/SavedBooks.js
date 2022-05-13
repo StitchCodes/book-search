be3e05beb22ef6a27ec0922e7e804bc9522a2c7c
StitchCodes <coderstitch@gmail.com>
2022-05-12 23:50:56 -0500

    Working on front mutations and resolvers. SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,42 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { GET_ME } from '../utils/queries';
+
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
+  // TODO: useMutation REMOVE_BOOK handleDeleteBook() replaces deleteBook()
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-  // TODO: use query GET_ME save to userData
-  // useMutation REMOVE_BOOK handleDeleteBook() replaces deleteBook()
+  const { username: userParam } = useParams();
+  const { loading, data } = useQuery(GET_ME, { variables: { username: userParam}});
 
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
+  setUserData(data?.me);  
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
-// TODO: USE MUTATION SAVE_BOOK
 // handleSaveBook() replaces saveBook()
 // bookID in try-catch with useState
 
 const SearchBooks = () => {
+
+  // USE MUTATION
+  const [saveBook, {error, data}] = useMutation(SAVE_BOOK)
+
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -33,7 +38,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await saveBook(searchInput);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -55,11 +60,15 @@ const SearchBooks = () => {
       console.error(err);
     }
   };
+  // TODO: USE MUTATION SAVE_BOOK
+  // handleSaveBook() replaces saveBook()
+  // bookID in try-catch with useState
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
